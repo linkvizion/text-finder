8931f7f75d7e1797aac5fdeaf474810c741e1fa3
linkvizion <linkvizion@gmail.com>
2020-11-14 20:30:42 +0200

    deleted randomly created class

--- src/main/java/com/example/textfinder/controller/SearchController.java
+++ src/main/java/com/example/textfinder/controller/SearchController.java
@@ -2,7 +2,6 @@ package com.example.textfinder.controller;
 
 import com.example.textfinder.model.SearchRequest;
 import com.example.textfinder.service.SearchService;
-import com.example.textfinder.service.SearchService1;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
