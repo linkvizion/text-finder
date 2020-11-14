a0e887ef7fa20cfff7e5e0036baffad97ba417bc
linkvizion <linkvizion@gmail.com>
2020-11-14 19:56:38 +0200

    initial commit

---
+++ src/main/java/com/example/textfinder/TextFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.textfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextFinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TextFinderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/textfinder/TextFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.textfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextFinderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
